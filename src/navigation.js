d5f63b5e41991763cd92c1509856ce1bc9f07d51
Sakeel Jawfer <37551196+sakeeljawfer@users.noreply.github.com>
2024-01-27 23:42:48 +0530

    footerlogo

--- src/navigation.js
+++ src/navigation.js
@@ -1,5 +1,6 @@
 import { getPermalink, getWorkPermalink } from './utils/permalinks';
 
+
 export const headerData = {
   links: [
     // {
@@ -158,7 +159,7 @@ export const footerData = {
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
   ],
   footNote: `
-    <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm bg-[url(https://onwidget.com/favicon/favicon-32x32.png)]"></span>
-     Axen Solutions· All rights reserved.
+    <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm bg-[url(src/assets/images/logo.png)]"></span>
+     © 2018 Axen. All Rights Reserved.
   `,
 };
