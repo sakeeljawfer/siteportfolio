b380f869e983ef27f8cc4cdd1e83a596090f9f9f
Sakeel Jawfer <37551196+sakeeljawfer@users.noreply.github.com>
2024-02-02 00:42:54 +0530

    servicespage

--- src/navigation.js
+++ src/navigation.js
@@ -159,7 +159,7 @@ export const footerData = {
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
   ],
   footNote: `
-    <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm bg-[url(src/assets/images/logo.png)]"></span>
+    <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm bg-[url(https://res.cloudinary.com/quadrate/image/upload/v1706813825/logo_uz2592.png)]"></span>
      © 2018 Axen. All Rights Reserved
   `,
 };
